ac653e95fdf6598dd295b8911754bad5d099690d
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-02-05 00:03:29 +0530

    adde stream with fs file

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import http from 'http';
-
+import fs from 'fs'
 const PORT =4000;
 
 const server = http.createServer((req,res)=>{
-    console.log('hello')
+    if (req.url !== '/') {
+        res.statusCode = 404;
+        res.end('Not Found');
+        return;
+    }else{
+        const readFile = fs.createReadStream('100mb-examplefile-com.txt')
+        readFile.pipe(res)
+    }
 })
 
 
