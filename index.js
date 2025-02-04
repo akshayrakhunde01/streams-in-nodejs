cef3582983c7ecf58639f25ee429c18e95e079ce
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-02-04 23:30:07 +0530

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(` hello node for streaming data`)
+import http from 'http';
+
+const PORT =4000;
+
+const server = http.createServer((req,res)=>{
+    console.log('hello')
+})
+
+
+
+
+server.listen(PORT,()=>{
+    console.log('listing on port',PORT)
+})
