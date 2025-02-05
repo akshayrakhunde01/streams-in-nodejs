2f7c9f5736f3eaf19ca0bb185260523391552df2
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-02-05 23:55:20 +0530

    updated video

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const server = http.createServer((req,res)=>{
         res.end('Not Found');
         return;
     }else{
-        const readFile = fs.createReadStream('100mb-examplefile-com.txt')
-        readFile.on('error',(error)=>{
-            res.end(`errrrr ${error.message}`)
-        })
-      
-            readFile.pipe(res)
+        const readFile = fs.readFileSync('200MB_1080P_THETESTDATA.COM_mp4_new.mp4')
+        // readFile.on('error',(error)=>{
+        //     res.end(`errrrr ${error.message}`)
+        // })
+        res.writeHead(200,{"content-type":'video/mp4'})
+            return res.end(readFile)
 
     }
 })
